228cf79aaeb35ee7b8a1a837623b5c732133c6d6
Unai Martínez Barredo <unaimartinezbarredo@gmail.com>
2021-04-03 09:40:37 +0100

    Real-time window resize, prepare for full-auto

--- discrop.py
+++ discrop.py
@@ -2,7 +2,7 @@ import math
 
 import obspython as obs
 
-SLOTS = 20
+SLOTS = 10 # Seems to be the maximum people allowed.
 
 # Discord call window measurements.
 TITLE_BAR = 22
@@ -13,12 +13,40 @@ CALLER_ASPECT = 16 / 9
 CALLER_SPACING = 8
 CALLER_BORDER = 3 # Inwards border when caller is talking.
 
+source = None
+order = []
+full_screen = False
 
-def script_description():
+
+def script_description(): # OBS script interface.
     return '<p style="color: orange"><strong>CAUTION:</strong> picking a Discord source from the menu below will <strong>irreversibly</strong> modify all related items!</p>'
 
 
-def script_properties():
+def script_load(settings): # OBS script interface.
+    pass # TODO.
+
+
+def script_update(settings): # OBS script interface.
+    global source
+    global order
+    global full_screen
+
+    obs.obs_source_release(source) # Doesn’t error even if source == None.
+    source = obs.obs_get_source_by_name(obs.obs_data_get_string(settings, 'discord_source'))
+
+    # Get caller order differences between Discord and OBS.
+    data = {}
+    for i in range(SLOTS):
+        name = obs.obs_data_get_string(settings, f'nickname{i}').lower()
+        if name.rstrip():# and obs.obs_data_get_bool(settings, f'has_video{i}'):
+            data[i] = name
+    # Discord sorts ‘ ’ before EOF, e.g. ‘foo bar’ > ‘foo’. Python doesn’t, but we can leverage the fact that ‘ ’ goes right before ‘!’.
+    order = sorted(data, key=lambda x: data[x] + '!')
+
+    full_screen = obs.obs_data_get_bool(settings, 'full_screen')
+
+
+def script_properties(): # OBS script interface.
     props = obs.obs_properties_create()
 
     grp = obs.obs_properties_create()
@@ -29,7 +57,6 @@ def script_properties():
 <ol>
   <li>In the voice channel where the call is happening, go to the bottom-right corner and select <em>Pop Out.</em></li>
   <li>In the top-right corner, ensure you’re in <em>Grid</em> mode (no caller appears big while the rest are small).</li>
-  <li>There too, under the three dots button, ensure <em>Show Non-Video Participants</em> is <strong>off.</strong></li>
 </ol>
 
 <h3>OBS capture source and scene set-up</h3>
@@ -37,15 +64,24 @@ def script_properties():
   <li>Create a <em>Window Capture</em> source and set it to capture your Discord call.</li>
   <li>Move and crop it until it’s the size and position you want a single caller’s video to be in the scene— don’t pay any attention to which area of the Discord call it shows, as that’s what this script does for you.</li>
   <li>Duplicate it <strong>(always as reference)</strong> and tweak it for every other caller, across scenes, etc.</li>
-  <li>Make sure the order of all Discord items in the <em>Sources</em> panel matches across your scenes, with the order in which you want the participants to show up (see next section).</li>
+  <li>Make sure the order of all Discord items in the <em>Sources</em> panel matches across your scenes, with the order in which you want the nicknames to show up (see next section).</li>
 </ol>
 
 <h3>Using this script</h3>
 <ol>
-  <li>Open the dropdown menu below, and pick the source that’s capturing the Discord call. <strong>CAUTION: this will irreversibly modify all items belonging to the source you pick!</strong></li>
-  <li>Fill the text fields with the Discord server nicknames of everyone that could have their video on during your Discord call, <strong>even if they aren’t online right now.</strong> Follow the same order you used with your Discord items in the <em>Sources</em> panel. You can use slots further down for people who might join the call with video, but shouldn’t appear in OBS.</li>
-  <li>Toggle the <em>Has video</em> checkboxes as people drop/rejoin!</li>
+  <li>Open the dropdown menu below, and pick the voice channel you’re in.</strong></li>
+  <li>Tick the <em>Full Screen</li> and <em>Show Non-Video Participants</em> checkboxes according to the state of your Discord call (on Discord, <em>Show Non-Video Participants</em> is located under the three dots button at the top right of the call window).</li>
+  <li>Open the next dropdown menu, and pick the source that’s capturing the Discord call. <strong>CAUTION: this will irreversibly modify all items belonging to the source you pick!</strong></li>
+  <li>Fill the text fields with the <strong>exact</strong> Discord server nicknames of everyone that you want to appear in your scene. Follow the same order you used with your Discord items in the <em>Sources</em> panel.</li>
 </ol>''')
+
+    p = obs.obs_properties_add_list(grp, 'voice_channel', 'Voice channel', obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    obs.obs_property_set_long_description(p, '<p>Discord server and voice/video channel where the call is happening.</p>')
+    obs.obs_property_list_add_string(p, '(Loading…)', None)
+    p = obs.obs_properties_add_bool(grp, 'full_screen', 'Full-screen')
+    obs.obs_property_set_long_description(p, '<p>Whether the Discord call window is in <em>Full Screen</em> mode</p>')
+    p = obs.obs_properties_add_bool(grp, 'show_nonvideo_participants', 'Show Non-Video Participants')
+    obs.obs_property_set_long_description(p, '<p>Whether the Discord call window has <em>Show Non-video Participants</em> on (under the three dots button at the top right corner)</p>')
 
     p = obs.obs_properties_add_list(grp, 'discord_source', 'Discord source', obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     obs.obs_property_set_long_description(p, '<p>Source that is capturing the Discord call. <strong>CAUTION: this will irreversibly modify all items belonging to the source you pick!</strong></p>')
@@ -59,42 +95,26 @@ def script_properties():
     for n in sorted(labels, key=lambda x: x.lower()):
         obs.obs_property_list_add_string(p, labels[n], n)
 
-    p = obs.obs_properties_add_bool(grp, 'full_screen', 'Full-screen')
-    obs.obs_property_set_long_description(p, '<p>Whether the Discord call window is in <em>Full Screen</em> mode</p>')
     obs.obs_properties_add_group(props, 'general', 'General', obs.OBS_GROUP_NORMAL, grp)
 
     grp = obs.obs_properties_create()
     for i in range(SLOTS):
-        p = obs.obs_properties_add_text(grp, f'nickname{i}', 'Nickname', obs.OBS_TEXT_DEFAULT)
+        p = obs.obs_properties_add_text(grp, f'nickname{i}', None, obs.OBS_TEXT_DEFAULT)
         obs.obs_property_set_long_description(p, '<p>Discord server nickname of the participant at the ' + ordinal(i + 1) + ' capture item from the top of the scene</p>')
-        p = obs.obs_properties_add_bool(grp, f'has_video{i}', 'Has video')
-        obs.obs_property_set_long_description(p, '<p>Whether the above participant is online and with video</p>')
-    obs.obs_properties_add_group(props, 'participants', 'Participants', obs.OBS_GROUP_NORMAL, grp)
+    obs.obs_properties_add_group(props, 'nicknames', 'Nicknames', obs.OBS_GROUP_NORMAL, grp)
 
     return props
 
 
-def script_update(settings):
+def script_tick(seconds): # OBS script interface.
 
     # Get Discord call window size.
-    source_name = obs.obs_data_get_string(settings, 'discord_source')
-    source = obs.obs_get_source_by_name(source_name)
     # TODO: Why are these 0 when no nickname is filled up?
     source_width = obs.obs_source_get_width(source)
     source_height = obs.obs_source_get_height(source)
-    obs.obs_source_release(source)
-
-    # Get caller order differences between Discord and OBS.
-    data = {}
-    for i in range(SLOTS):
-        name = obs.obs_data_get_string(settings, f'nickname{i}').lower()
-        if name.rstrip() and obs.obs_data_get_bool(settings, f'has_video{i}'):
-            # Discord sorts ‘ ’ before EOF, e.g. ‘foo bar’ > ‘foo’. Python doesn’t, but we can leverage the fact that ‘ ’ goes right before ‘!’.
-            data[i] = name + '!'
-    order = sorted(data, key=lambda x: data[x])
 
     margin_top = MARGIN_TOP
-    if not obs.obs_data_get_bool(settings, 'full_screen'):
+    if not full_screen:
         margin_top = margin_top + TITLE_BAR
 
     # Get Discord call layout distribution and caller size.
@@ -150,8 +170,7 @@ def script_update(settings):
         j = 0
         for i in reversed(range(len(items))):
             item = items[i]
-            source = obs.obs_sceneitem_get_source(item) # Shouldn’t be released.
-            if obs.obs_source_get_name(source) == source_name:
+            if obs.obs_sceneitem_get_source(item) == source: # Shouldn’t be released.
                 visible = False
                 index = None
                 for k, v in enumerate(order):
