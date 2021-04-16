98948f87a73ed3131ba775cb8d07353119622330
Unai Martínez Barredo <unaimartinezbarredo@gmail.com>
2021-04-16 21:03:10 +0100

    wip

--- sdk_test.py
+++ sdk_test.py
@@ -3,20 +3,22 @@ import time
 
 import discordsdk
 
+
 with open(os.path.join(os.path.dirname(__file__), '.application_id')) as f:
     app = discordsdk.Discord(int(f.read().rstrip()), discordsdk.CreateFlags.default)
 
-user_manager = app.get_user_manager()
+lobbymgr = app.get_lobby_manager()
 
 
-def on_curr_user_update():
-    user = user_manager.get_current_user()
-    print(f'Current user : {user.username}#{user.discriminator}')
+def lobby_search(result):
+    if result == discordsdk.Result.ok:
+        print(lobbymgr.lobby_count())
 
 
-user_manager.on_current_user_update = on_curr_user_update
+query = lobbymgr.get_search_query()
+query.limit(10)
+lobbymgr.search(search, lobby_search)
 
-# Don't forget to call run_callbacks
 while 1:
     time.sleep(1/10)
     app.run_callbacks()
